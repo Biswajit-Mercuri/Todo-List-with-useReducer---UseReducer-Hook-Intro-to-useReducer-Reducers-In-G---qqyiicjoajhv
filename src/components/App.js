d2e5275a60b543e812c95f3316642c16374fcca7
Biswajit Deb Barman <108471872+Biswajit-Mercuri@users.noreply.github.com>
2022-10-27 23:21:56 +0530

    lets run

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React, { useReducer } from "react";
-import { todoReducer } from "../reducers/todo";
-import "../styles/App.css";
-import { AddTodo } from "./AddTodo";
-import Navbar from "./Navbar";
-import { Todo } from "./Todo";
+import React, { useReducer } from 'react'
+import { todoReducer } from '../reducers/todo';
+
+import '../styles/App.css';
+
+import { AddTodo } from './AddTodo';
+import { Todo } from './Todo';
+
+import { Navbar } from './Navbar';
+
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, {
-    /* Initial todo state array, could be an array for storing indivial todo objects */
-    todos: [],
-  });
+  const [state, dispatch] = useReducer(todoReducer, {/* Initial todo state array, could be an array for storing indivial todo objects */ todos: [] })
+
+
   return (
     <div id="main">
       <Navbar />
       <AddTodo state={state} dispatch={dispatch} />
       {/* Render list of Todo Components here */}
     </div>
-  );
-};
+  )
+}
+
+
 export default App;
